5d6c5b1f73257de7fda076f6e79247b467b6acd3
parag95 <paragjain95@gmail.com>
2015-10-11 15:04:41 +0530

    Mod 4 final

--- RestaurantManager/RestaurantManager.UniversalWindows/OrderPage.xaml.cs
+++ RestaurantManager/RestaurantManager.UniversalWindows/OrderPage.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
@@ -12,6 +13,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+ 
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -32,5 +34,6 @@ namespace RestaurantManager.UniversalWindows
             Frame.Navigate(typeof(MainPage));
         }
 
+       
     }
 }
